a69a630120955c90c82696148b54af01da3bab12
Cosimo Cecchi <cosimoc@gnome.org>
2012-08-16 23:38:01 +0200

    notification: use app-notification as style class
    Adwaita now provides an unified style class for all the app
    notifications; use it to avoid duplication of the CSS.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=682046

--- src/gtk-notification.c
+++ src/gtk-notification.c
@@ -109,7 +109,7 @@ gtk_notification_init (GtkNotification *notification)
   GtkNotificationPrivate *priv;
 
   context = gtk_widget_get_style_context (GTK_WIDGET (notification));
-  gtk_style_context_add_class (context, "contacts-notification");
+  gtk_style_context_add_class (context, "app-notification");
 
 
   gtk_widget_set_halign (GTK_WIDGET (notification), GTK_ALIGN_CENTER);
