848a43906bdef7ee7be9b9353700bbc8d1fe2aa4
Erick Pérez Castellanos <erick.red@gmail.com>
2012-12-04 18:01:40 -0500

    Added libgd module

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p m4
 autopoint --force
+git submodule update --init --recursive
 AUTOPOINT='intltoolize --automake --copy' autoreconf -fiv -Wall || exit
 ./configure --enable-maintainer-mode "$@"
