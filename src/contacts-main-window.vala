GNOME/gnome-contacts

commit 3e84850def40672cf22307933c75fa07c550dd31

main-window: Allow Escape to cancel selection mode
Fixes: https://gitlab.gnome.org/GNOME/gnome-contacts/-/issues/354

data/ui/contacts-main-window.ui
@@ -99,7 +99,7 @@ SPDX-License-Identifier: GPL-2.0-or-later
         <child>
           <object class="GtkShortcut">
             <property name="trigger">Escape</property>
-            <property name="action">action(win.edit-contact-cancel)</property>
+            <property name="action">action(win.cancel)</property>
           </object>
         </child>
         <child>
@@ -157,6 +157,7 @@ SPDX-License-Identifier: GPL-2.0-or-later
                             <property name="label" translatable="yes">Cancel</property>
                             <property name="tooltip_text" translatable="yes">Cancel Selection</property>
                             <property name="can-shrink">True</property>
+                            <property name="action-name">win.cancel-selection</property>
                           </object>
                         </child>
                       </object>

src/contacts-main-window.vala
@@ -11,13 +11,14 @@ using Folks;
 public class Contacts.MainWindow : Adw.ApplicationWindow {
 
   private const GLib.ActionEntry[] ACTION_ENTRIES = {
+    { "cancel", cancel_action },
     { "new-contact", new_contact },
     { "edit-contact", edit_contact },
-    { "edit-contact-cancel", edit_contact_cancel },
     { "edit-contact-save", edit_contact_save },
     { "focus-search", focus_search },
     { "mark-favorite", mark_favorite },
     { "unmark-favorite", unmark_favorite },
+    { "cancel-selection", cancel_selection_action },
     { "link-marked-contacts", link_marked_contacts },
     { "delete-marked-contacts", delete_marked_contacts },
     { "export-marked-contacts", export_marked_contacts },
@@ -106,7 +107,6 @@ public class Contacts.MainWindow : Adw.ApplicationWindow {
     // Widgets
     create_list_pane ();
     create_contact_pane ();
-    connect_button_signals ();
     restore_window_state ();
 
     // Make sure we notify in the UI when this is an unstable development build
@@ -353,9 +353,24 @@ public class Contacts.MainWindow : Adw.ApplicationWindow {
     this.contact_pane_page.title = "";
   }
 
-  private void edit_contact_cancel (SimpleAction action, GLib.Variant? parameter) {
-    if (this.state != UiState.CREATING && this.state != UiState.UPDATING)
-      return;
+  // Action that tries to handle generally canceling something, e.g. when pressing Escape
+  private void cancel_action (SimpleAction action, GLib.Variant? parameter) {
+    switch (this.state) {
+      case UiState.CREATING:
+      case UiState.UPDATING:
+        edit_contact_cancel ();
+        break;
+      case UiState.SELECTING:
+        cancel_selection_action (null, null);
+        break;
+      default:
+        // Do nothing
+        break;
+    }
+  }
+
+  private void edit_contact_cancel ()
+      requires (this.state == UiState.CREATING || this.state == UiState.UPDATING) {
 
     if (this.state == UiState.CREATING) {
       this.state = UiState.NORMAL;
@@ -405,15 +420,13 @@ public class Contacts.MainWindow : Adw.ApplicationWindow {
     this.filter_entry.set_text (query);
   }
 
-  private void connect_button_signals () {
-    this.select_cancel_button.clicked.connect (() => {
-        this.selection_model.marked.unselect_all ();
-        if (this.selection_model.selected.get_selected () != Gtk.INVALID_LIST_POSITION) {
-            this.state = UiState.SHOWING;
-        } else {
-            this.state = UiState.NORMAL;
-        }
-    });
+  private void cancel_selection_action (SimpleAction? action, GLib.Variant? parameter) {
+    this.selection_model.marked.unselect_all ();
+    if (this.selection_model.selected.get_selected () != Gtk.INVALID_LIST_POSITION) {
+      this.state = UiState.SHOWING;
+    } else {
+      this.state = UiState.NORMAL;
+    }
   }
 
   public override bool close_request () {

commit 81108d9331cea799a33459320134f2953a72bbe5

main-window: Make sure to rename "cancel" action eveyrwhere
In commit 3e84850def, we grouped together the several "cancel-*" actions
into a single "cancel" action. The idea being that we want to make sure
the "wrong" cancel method isn't being called.

However, in doing so, we forgot to rename a couple of instances,
effectively breaking the "Cancel" button in some cases in the UI. This
commit now pushes through the rename in those cases too.

Fixes: 3e84850def ("main-window: Allow Escape to cancel selection mode")

data/ui/contacts-main-window.ui
@@ -274,7 +274,7 @@ SPDX-License-Identifier: GPL-2.0-or-later
                             <property name="visible">False</property>
                             <property name="label" translatable="yes">_Cancel</property>
                             <property name="use_underline">True</property>
-                            <property name="action-name">win.edit-contact-cancel</property>
+                            <property name="action-name">win.cancel</property>
                             <property name="can-shrink">True</property>
                           </object>
                         </child>

src/contacts-main-window.vala
@@ -455,7 +455,7 @@ public class Contacts.MainWindow : Adw.ApplicationWindow {
     string response = yield dialog.choose (this, null);
 
     if (response == "discard") {
-      activate_action ("edit-contact-cancel", null);
+      activate_action ("cancel", null);
       return true;
     } else {
       return false;